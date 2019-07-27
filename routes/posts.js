ec4f5cb2e58d586872a3b3e9bf1d2b72e8da3027
Dominic Torruellas <dom@domtorr.com>
2019-07-27 11:27:41 -0700

    - [x] - /api/posts/:id/comments - GET - Comments by post id

--- routes/posts.js
+++ routes/posts.js
@@ -30,4 +30,23 @@ router.get('/:id', (req, res) => {
     );
 });
 
+router.get('/:id/comments', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const postWasFound = await db.findById(id).then(post => post.length > 0);
+
+    return postWasFound
+      ? await db
+          .findPostComments(id)
+          .then(comments => res.status(200).json(comments))
+      : res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist' });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: 'The comments information could not be retrieved.' });
+  }
+});
+
 module.exports = router;
