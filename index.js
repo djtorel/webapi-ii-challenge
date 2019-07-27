3b862f85bf54430eb22542c4849878bf5d522515
Dominic Torruellas <dom@domtorr.com>
2019-07-27 09:53:04 -0700

    - [x] - /api/posts - GET - all posts

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const postsRoutes = require('./routes/posts');
+
+const server = express();
+
+server.use('/api/posts', postsRoutes);
+
+server.listen(8000, () => console.log('API running on port 8000'));
