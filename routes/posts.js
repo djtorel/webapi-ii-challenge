41b903d750b0da8818fcc54ef3b2a2c0cc01e49b
Dominic Torruellas <dom@domtorr.com>
2019-07-27 10:14:54 -0700

    - [x] - /api/posts/:id - GET - Post by Id

--- routes/posts.js
+++ routes/posts.js
@@ -14,4 +14,20 @@ router.get('/', (req, res) => {
     );
 });
 
+router.get('/:id', (req, res) => {
+  db.findById(req.params.id)
+    .then(post =>
+      post.length > 0
+        ? res.status(200).json(post)
+        : res
+            .status(404)
+            .json({ message: 'The post with the specified ID does not exist.' })
+    )
+    .catch(() =>
+      res
+        .status(500)
+        .json({ error: 'The post information could not be retrieved.' })
+    );
+});
+
 module.exports = router;
